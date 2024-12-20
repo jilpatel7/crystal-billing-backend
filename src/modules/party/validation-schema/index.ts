d2571f72d344bc7ee1f03ad24621133307b81967
qudio-dev <dev.qudio@gmail.com>
2024-12-20 11:57:17 +0530

    SL : Fix validations

--- src/modules/party/validation-schema/index.ts
+++ src/modules/party/validation-schema/index.ts
@@ -2,10 +2,10 @@ import Joi from "joi";
 
 export const partySchema = Joi.object({
   id: Joi.number().when('$isUpdate', { is: true, then: Joi.required(), otherwise: Joi.optional() }),
-  gstin_no: Joi.string().trim().length(15),
+  gstin_no: Joi.string().trim().length(15).optional(),
   name: Joi.string().trim().required(),
-  email: Joi.string().trim().email().max(50),
-  personal_phone: Joi.string().trim(),
+  email: Joi.string().trim().email().max(50).optional(),
+  personal_phone: Joi.string().trim().optional(),
   office_phone: Joi.string().trim().required(),
   logo: Joi.string(),
   price_per_caret: Joi.number().min(0).required()
